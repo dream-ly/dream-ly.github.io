813b2e9317c95a2f94b8d39d03ac05368f657fd2
dream-ly <60845512+dream-ly@users.noreply.github.com>
2025-08-12 16:54:29 -0700

    update

--- selected-pub.js
+++
@@ -1,6 +0,0 @@
-function showResearchTab(tab) {
-    console.log("showResearchTab called with tab:", tab); // Debugging line
-    document.getElementById("research-selected").style.display = tab === 'selected' ? 'block' : 'none';
-    document.getElementById("research-all").style.display = tab === 'all' ? 'block' : 'none';
-    document.getElementById("research-hai").style.display = tab === 'hai' ? 'block' : 'none';
-}
